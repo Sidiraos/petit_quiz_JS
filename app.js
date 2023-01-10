040a8e309d8b826330677c5f65d1b9979df6c0d1
Sidiraos <raossidi@gmail.com>
2023-01-10 17:41:28 +0300

    modification Js v 1.1.0

--- app.js
+++ app.js
@@ -1,76 +1,83 @@
-const responses = ["c", "a", "b", "a", "c"];;
+const responses = ["c", "a", "b", "a", "c"];
 const emojis = ["✔️", "✨", "👀", "😭", "👎"];
-const falseResponse = "background: linear-gradient(to right, #f857a6, #ff5858);" ;
-const trueResponse = "background: linear-gradient(to right, #11998e, #38ef7d);" ;
-const check = document.querySelector('#valider');
-// reponses des users
-const radioButtons = document.querySelectorAll('input[type=radio]:checked') 
+const falseResponse =
+  "background: linear-gradient(to right, #f857a6, #ff5858);";
+const trueResponse = "background: linear-gradient(to right, #11998e, #38ef7d);";
+const check = document.querySelector("#valider");
 
-function addUserResponses() {
-    let userValues = [] ;
-    for (let radioButton of radioButtons){
-        if(radioButton.checked) {
-            console.log(radioButton.value) ;
-            userValues.push(radioButton.value) ;
-        }
-        else {
-            console.log("a radio button is not checked")
-        }
+function addUserResponses(radioButtons) {
+  let userValues = [];
+  radioButtons.forEach(function (button, index) {
+    console.log("button value : ", button.value);
+    if (button.value === responses[index]) {
+      userValues.push(true);
+    } else {
+      userValues.push(false);
     }
-    console.log(userValues) ;
-    return userValues ;
+  });
+
+  return userValues;
 }
 
-// evenement click du bouton pour envoyer les reponses 
-check.addEventListener('click' , () => {
-    let score = 0 ;
-// ajouter les reponses de l'utilisateur
-    let userValues = addUserResponses() ;
-// changer le background id true or false
-    for (let i = 0 ; i < userValues.length ; i++) {
-        if (userValues[i] == responses[i]) {
-            document.getElementById(`q${i+1}`).style = trueResponse;
-            score++ ;
-        }
-        else {
-            document.getElementById(`q${i+1}`).style = falseResponse ;
-        }
+function bgChangingWithUserResponse(userValues) {
+  userValues.forEach(function (value, i) {
+    console.log("value ", value);
+    if (value) {
+      document.getElementById(`q${i + 1}`).style = trueResponse;
+    } else {
+      document.getElementById(`q${i + 1}`).style = falseResponse;
     }
+  });
+}
 
-    // feedback score
-    // score
-    console.log("le score : " , score) ;
-    let motivationSentence;
-    let feedback = "Retentez une autre réponse dans les cases rouges, puis re-validez !" ;
-    let note = (score / responses.length).toFixed(1)
-    
-    if( note <= 0.2) {
-        motivationSentence = emojis[3] + " Peut mieux faire ! " + emojis[3] ;
-    }
-    else if ( note <= 0.4) {
-        motivationSentence = emojis[2] + " Il reste quelques erreurs. " + emojis[3] ;
-    }
-    else if ( note <= 0.6) {
-        motivationSentence = emojis[1] + " Encore un effort ... " + emojis[2] ;
-    }
-    else if ( note == 1) {
-        motivationSentence = emojis[0] + " Bravo, c'est un sans faute ! " + emojis[0] ;
-        feedback = "Quelle culture ..." ;
-    }
-    else {
-        motivationSentence = emojis[1] + " Vous y êtes presque ! " + emojis[1] ;
-    }
-    
-    document.getElementById('feedback').innerHTML = `<p class = 'fs-4'>${motivationSentence} </p> <p class = 'fs-4'>Score : <span class = 'fw-bold'> ${score} / ${responses.length} </span> </p> <p class = 'fw-normal'>${feedback}</p>`
+function showScore(userValues) {
+  let score = userValues.filter((elt) => elt === true).length;
+  console.log("le score : ", score);
+  let motivationSentence;
+  let feedback =
+    "Retentez une autre réponse dans les cases rouges, puis re-validez !";
+  let note = (score / responses.length).toFixed(1);
+
+  if (note <= 0.2) {
+    motivationSentence = emojis[3] + " Peut mieux faire ! " + emojis[3];
+  } else if (note <= 0.4) {
+    motivationSentence = emojis[2] + " Il reste quelques erreurs. " + emojis[3];
+  } else if (note <= 0.6) {
+    motivationSentence = emojis[1] + " Encore un effort ... " + emojis[2];
+  } else if (note == 1) {
+    motivationSentence =
+      emojis[0] + " Bravo, c'est un sans faute ! " + emojis[0];
+    feedback = "Quelle culture ...";
+  } else {
+    motivationSentence = emojis[1] + " Vous y êtes presque ! " + emojis[1];
+  }
+
+  document.getElementById(
+    "feedback"
+  ).innerHTML = `<p class = 'fs-4'>${motivationSentence} </p> <p class = 'fs-4'>Score : <span class = 'fw-bold'> ${score} / ${responses.length} </span> </p> <p class = 'fw-normal'>${feedback}</p>`;
+}
+
+// programme principale
+// evenement click du bouton pour envoyer les reponses
+check.addEventListener("click", () => {
+  const radioButtons = document.querySelectorAll("input[type=radio]:checked");
+  console.log("valeurs checké ", radioButtons);
+  let userValues = addUserResponses(radioButtons);
+  console.log(userValues);
+  bgChangingWithUserResponse(userValues);
+  showScore(userValues);
 });
 
 // evenement changement de radio background disparait
-for (let radioButton of radioButtons) {
-    radioButton.addEventListener('change' , () => {
-        console.log('i changed radio button') ;
-        const parentClass = radioButton.closest('.col-12') ;
-        parentClass.style = "background : none ;"
-        console.log(parentClass)
-})
-}
-
+const radioButtons = document.querySelectorAll("input[type=radio]");
+function bgChangingWithRadio() {
+    radioButtons.forEach(radioButton => {
+      radioButton.addEventListener("change", () => {
+        console.log("i changed radio button");
+        const parentClass = radioButton.closest(".col-12");
+        parentClass.style = "background : none ;";
+        console.log(radioButton);
+      });
+    });
+}  
+bgChangingWithRadio();
